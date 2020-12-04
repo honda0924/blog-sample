6ae1c6ba3647125edb16e0c992dc256de77bc1c5
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2020-12-05 00:03:03 +0900

    create_view

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,14 @@
 class BlogsController < ApplicationController
+  def new
+    @blog=Blog.new
+  end
+  def def create
+    Blog.create(blog_params)
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
